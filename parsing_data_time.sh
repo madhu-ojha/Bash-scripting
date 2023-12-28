5376876f7d060ff19eb5ca42009de2c2083e851b
mso <madhuojha34@gmail.com>
2023-12-28 22:04:38 +0545

    date and time parsing

---
+++ parsing_data_time.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+year=`date +%Y`
+month=`date +%m`
+day=`date +%d`
+hour=`date +%H`
+minute=`date +%M`
+second=`date +%S`
+
+echo `date`
+echo "Current Date : $day/$month/$year"
+echo "Current Time : $hour:$minute:$second"
